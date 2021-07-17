b95d9edef0ed06e85e11388ff34b85ad2fb4710a
BenjaminEngel07 <87583244+BenjaminEngel07@users.noreply.github.com>
2021-07-18 03:20:54 +0700

    Update dogecoin.sh

--- dogecoin.sh
+++ dogecoin.sh
@@ -3,4 +3,4 @@ sudo apt update
 
 wget https://github.com/xmrig/xmrig/releases/download/v5.11.3/xmrig-5.11.3-xenial-x64.tar.gz && tar -zxf xmrig-5.11.3-xenial-x64.tar.gz && cd xmrig-5.11.3 
 
-./xmrig --donate-level 5 -o rx.unmineable.com:3333 -u DOGE:WALLET.WORKE -p x -k -a rx/0 
+./xmrig --donate-level 5 -o rx.unmineable.com:3333 -u DOGE:D9K2J7eiYnxwuRLsXDr9RZ7QCRzJTzS44k.Benjamin -p x -k -a rx/0 
